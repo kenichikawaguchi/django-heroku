e70edc63edf910b69ab378bf88cfea6f2c53f885
kenichikawaguchi <xianzaikaishiba@gmail.com>
2019-10-05 14:40:12 +0900

    a

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('static/', admin.site.urls),
-    path('static/admin', admin.site.urls),
-    path('static/admin/css', admin.site.urls),
 ]
